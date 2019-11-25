23d259845fed4d13cbb057754d3fe9080380664e
yuslianto <yuslianto56@gmail.com>
2019-11-25 16:46:21 +0700

    navigation#04 adding custom fonts

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
@@ -10,7 +10,7 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+        <Text style={styles.articleTitle}>Home Screen</Text>
         <Icon
           name={Platform.OS === 'ios' ? 'ios-add' : 'md-add'}
           color="black"
@@ -29,7 +29,7 @@ class ProfileScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
+        <Text style={styles.articleText}>Profile Screen</Text>
         <Button
           title="go to home screen"
           onPress={()=>this.props.navigation.navigate('Home')}  
@@ -46,6 +46,19 @@ const AppNavigator = createStackNavigator({
   Profile: {
     screen: ProfileScreen,
   },
+});
+
+const styles = StyleSheet.create({
+  articleTitle: {
+      fontSize: 23,
+      color: '#323232',
+      fontFamily: 'Roboto-Bold',
+  },
+  articleText: {
+      fontSize: 14,
+      lineHeight: 20,
+      fontFamily: 'Roboto-Light'
+  }
 });
 
 export default createAppContainer(AppNavigator);
