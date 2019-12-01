b324c4bbf13cf27a3943a375ec024424524e9d97
yuslianto <yuslianto56@gmail.com>
2019-12-01 09:08:58 +0700

    navigation fixing the shadows

--- src/Molekul/CategoryGridTile/CategoryGridTile.js
+++ src/Molekul/CategoryGridTile/CategoryGridTile.js
@@ -35,7 +35,9 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
+        overflow: Platform.OS === 'android' && Platform.Version >= 21 
+            ? 'hidden' 
+            : 'visible',
     },
     container: {
         flex: 1,
@@ -44,7 +46,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2},
         shadowRadius: 10,
-        elevation: 3,
+        elevation: 10,
         padding: 10,justifyContent: 'flex-end',
         alignItems: 'flex-end'
     },
