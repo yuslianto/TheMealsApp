06f4a3e7c0e137859d2413057e3fc43734aa5cbc
yuslianto <yuslianto56@gmail.com>
2019-12-03 09:58:07 +0700

    navigation#29 adding a defaulttext component

--- src/Molekul/MealItem/MealItem.js
+++ src/Molekul/MealItem/MealItem.js
@@ -7,6 +7,8 @@ import {
     StyleSheet
 } from "react-native";
 
+import DefaultText from '../../Atom/DefaultText/DefaultText';
+
 const MealItem = (props) => {
     return (
         <View style={styles.mealItem}>
@@ -26,9 +28,9 @@ const MealItem = (props) => {
                         </ImageBackground>
                     </View>
                     <View style={{...styles.mealRow, ...styles.mealDetail}}>
-                        <Text>{props.duration}m</Text>
-                        <Text>{props.complexity.toUpperCase()}</Text>
-                        <Text>{props.affordability.toUpperCase()}</Text>
+                        <DefaultText>{props.duration}m</DefaultText>
+                        <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+                        <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
                     </View>
                 </View>
             </TouchableOpacity>
