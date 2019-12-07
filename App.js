a2f24b6d43aee6a3be0b94c32419f899dd5169a6
yuslianto <yuslianto56@gmail.com>
2019-12-07 17:24:10 +0700

    state-redux#03 redux and store setup

--- App.js
+++ App.js
@@ -6,16 +6,27 @@ import {
     StyleSheet
 } from "react-native";
 import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import MealsNavigator from './src/Template/navigation/MealsNavigator';
+import mealsReducer from './src/Template/store/reducers/meals';
 
 //enableScreens();
 
 YellowBox.ignoreWarnings(['Warning: componentWillReceiveProps has been renamed'])
 
+const rootReducer = combineReducers({
+    meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
+
 const App = (props) => {
     return (
-        <MealsNavigator/>
+        <Provider store={store}>
+            <MealsNavigator/>
+        </Provider>
     );
 };
 
