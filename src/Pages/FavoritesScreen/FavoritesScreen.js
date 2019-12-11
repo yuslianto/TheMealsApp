387433ceb21995115c51ac5d6cefc43666e50d60
yuslianto <yuslianto56@gmail.com>
2019-12-11 10:12:00 +0700

    state-redux#08 rendering a fallback text-for-no-favs

--- src/Pages/FavoritesScreen/FavoritesScreen.js
+++ src/Pages/FavoritesScreen/FavoritesScreen.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { 
-    StyleSheet
+    StyleSheet,
+    View
 } from "react-native";
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import HeaderButton from '../../Molekul/HeaderButton/HeaderButton';
 import MealList from '../../Molekul/MealList/MealList';
+import DefaultText from '../../Atom/DefaultText/DefaultText';
 
 const FavoritesScreen = (props) => {
 
     const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+    if (favMeals.length === 0 || !favMeals) {
+        return(
+            <View style={styles.content}>
+                <DefaultText>No favorite meals found. Start adding some !!!</DefaultText>
+            </View>
+        );
+    }
 
     return <MealList listData={favMeals} navigation={props.navigation}/>
 
@@ -35,7 +45,7 @@ FavoritesScreen.navigationOptions = (navData) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
+    content: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
