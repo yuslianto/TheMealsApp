8fb3b8b19ec0e70dc22424951cbe6bc56ea10921
yuslianto <yuslianto56@gmail.com>
2019-12-11 10:29:18 +0700

    state-redux#09 adding filtering logic

--- src/Template/store/actions/meals.js
+++ src/Template/store/actions/meals.js
@@ -1,8 +1,13 @@
 export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE';
+export const SET_FILTERS = 'SET_FILTERS';
 
 export const toggleFavorite = (id) => {
     return { 
         type: TOGGLE_FAVORITE, 
         mealId: id 
     };
+};
+
+export const setFilters = filterSttings => {
+    return { type: SET_FILTERS, filters: filterSttings};
 };
